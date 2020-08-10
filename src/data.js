77949ee9f6eebd63873280719212592dd82d9f61
wildpow <element1028@gmail.com>
2020-08-10 08:26:43 -0700

    Landing page setup

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    description: "Whatever you do in bed, Sealy supports it. Proudly built in America for over 130 years, Sealy designs high-quality mattresses that are built to last. Posturepedic technology targeting the heaviest points in your body and delivering advanced support there so your body stays level while you sleep.",
+    headingImg: {
+      alt: "Sealy logo",
+      url: "https://www.datocms-assets.com/10836/1572653401-sealyheadingimage.png",
+    },
+    heroImg: {
+      alt: null,
+      title: null,
+      url: "https://www.datocms-assets.com/10836/1572653408-sealyheroimage.jpg",
+    }
+  }
+]
