2beabd2a1cb32debbeb0d6c533c5cd5c260224bc
wildpow <element1028@gmail.com>
2020-08-10 09:25:57 -0700

    completed data file

--- src/data.js
+++ src/data.js
@@ -1,14 +1,156 @@
 const data = [
   {
-    description: "Whatever you do in bed, Sealy supports it. Proudly built in America for over 130 years, Sealy designs high-quality mattresses that are built to last. Posturepedic technology targeting the heaviest points in your body and delivering advanced support there so your body stays level while you sleep.",
+    description:
+      "Whatever you do in bed, Sealy supports it. Proudly built in America for over 130 years, Sealy designs high-quality mattresses that are built to last. Posturepedic technology targeting the heaviest points in your body and delivering advanced support there so your body stays level while you sleep.",
+    title: "sealy",
     headingImg: {
-      alt: "Sealy logo",
-      url: "https://www.datocms-assets.com/10836/1572653401-sealyheadingimage.png",
+      alt: "Sealy a proud supporter of you",
+      title: "Sealy Logo",
+      url: "./images/sealyheadingimage.png",
     },
     heroImg: {
-      alt: null,
-      title: null,
-      url: "https://www.datocms-assets.com/10836/1572653408-sealyheroimage.jpg",
-    }
-  }
+      alt: "Sealy mattress in a bedroom",
+      title: "Sealy Mattress",
+      url: "./images/sealyheroimage.jpg",
+    },
+    tabBox: [
+      {
+        id: "DatoCmsTabBox-1687236-en",
+        name: "sealy sublines",
+        title: "Sealy Response",
+        topText: "Which Sealy line is right for you?",
+        topImage: {
+          alt: "Sealy Collections",
+          height: 126,
+          title: "Sealy Collections",
+          url: "./images/sealycollectionstopimage.png",
+          width: 458,
+        },
+        box: [
+          {
+            title: "Sealy Response",
+            description:
+              "The Sealy Response collection uses advanced, precisely engineered coil systems to promote proper alignment while delivering a “sleep-on-top” feeling. Exclusive Posturepedic Technology provides the stable, all-over support you want, with reinforced support where you need it most.",
+            picture: {
+              alt: "Sealy Response mattress",
+              title: "Sealy Response",
+              url: "./images/sealycollectionresponse.jpg",
+            },
+          },
+          {
+            title: "Sealy Hybrid",
+            description:
+              "Top-Rated Sealy Hybrids use exclusive Posturepedic Technology with reinforced support under the heaviest part of your body having 20% more coils—while also giving you that “sleep-on-top” feeling and the comfort you expect.  All Sealy Hybrids are topped off with conforming gel memory foam.",
+            picture: {
+              alt: "Sealy Hybrid mattress",
+              title: "Sealy Hybrid",
+              url: "./images/sealycollectionhybrid.jpg",
+            },
+          },
+          {
+            title: "Sealy Golden Elegance",
+            description:
+              "Golden Elegance collection of mattress sets a high bar for what a value mattress can be. At the heart of a Sealy mattress is the support you need for a great night's sleep. Sealy combines smart innovation, precise engineering, and industry-leading testing to ensure quality and durability.",
+            picture: {
+              alt: "Sealy Golden Elegance Mattress",
+              title: "Sealy Golden Elegance",
+              url: "./images/sealycollectiongoldenelegance.jpg",
+            },
+          },
+        ],
+      },
+      {
+        id: "DatoCmsTabBox-1687241-en",
+        name: "sealy base",
+        title: "sealy",
+        topText:
+          "Experience Sealy support with an adjustable power base to enhance your sleep or a flat foundation.",
+        topImage: {
+          alt: "Sealy foundations",
+          height: 126,
+          title: "Sealy foundations",
+          url: "./images/sealyfoundationstopimage.png",
+          width: 458,
+        },
+        box: [
+          {
+            title: "Ease",
+            description:
+              "Ease has all the features you need in a power base. With nearly unlimited ergonomic positions and presets, the Ease is an essential part of a complete and seamless sleep experience.",
+            picture: {
+              alt: "Sealy Ease adjustable base",
+              title: "Sealy Ease adjustable base",
+              url: "./images/sealyfoundationease.jpg",
+            },
+          },
+          {
+            title: "Clarity II",
+            description:
+              "The Rize Clarity II power base offers the options you want in an adjustable base at an attractive price point.  Including premium technologies like 850lb lift capacity, USB ports, under-bed lighting, and massage.",
+            picture: {
+              alt: "Rize Clarity II power base",
+              title: "Rize Clarity II power base",
+              url: "./images/sealyfoundationclarityii.jpg",
+            },
+          },
+          {
+            title: "Tempur-Ergo",
+            description:
+              "The TEMPUR-Ergo power base is an essential part of a holistic sleep system, combining premium technologies like QuietMode, USB ports, and under-bed lighting.",
+            picture: {
+              alt: "Tempur-Ergo power base",
+              title: "Tempur-Ergo",
+              url: "./images/sealyfoundationergo.jpg",
+            },
+          },
+          {
+            title: "Flat Foundation",
+            description:
+              "A solid surface foundation is engineered to provide years of stationary support. Designed and upholstered to complement our mattresses and your bedroom decor. The flat foundation comes in ultra-low (2”), low profile (5″), and high profile (9″).",
+            picture: {
+              alt: "Sealy Flat Foundation",
+              title: "Flat Foundation",
+              url: "./images/sealyfoundationflat.jpg",
+            },
+          },
+        ],
+      },
+      {
+        id: "DatoCmsTabBox-1687244-en",
+        name: "sealy top of bed",
+        title: "sealy",
+        topText:
+          "We’ve got your new Sealy mattress covered on the finishing touches.",
+        topImage: {
+          alt: "Top it all off",
+          height: 126,
+          title: "Sealy top it all off",
+          url: "./images/sealytopitofftopimage.png",
+          width: 458,
+        },
+        box: [
+          {
+            title: "Mattress Protector",
+            description:
+              "Protect-A-Bed premium cool Tencel mattress protectors will extend the life of your mattress and keep your sleeping area clean and cool for years to come.",
+            picture: {
+              alt: "Protect-A-Bed premium cool Tencel mattress protectors",
+              title: "Protect-A-Bed premium cool Tencel",
+              url: "./images/sealypab.png",
+            },
+          },
+          {
+            title: "DreamFit Sheets",
+            description:
+              "Really great sheets, by all definitions, are sheets that look beautiful, feel great, have lasting softness and durability, and STAY PUT on the mattress all night long. DreamFit has it covered.",
+            picture: {
+              alt: "DreamFit Sheets",
+              title: "DreamFit Sheets",
+              url: "./images/sealydreamfit.jpg",
+            },
+          },
+        ],
+      },
+    ],
+  },
 ]
