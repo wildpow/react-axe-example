279672452162ac01dc92e1f11a68f0f56c4b3a8d
wildpow <element1028@gmail.com>
2020-08-16 13:52:21 -0700

    added react-axe and gatsby plugin for it

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-react-axe",
+      options: {
+        debounce: 1000,
+        showInProduction: false,
+      },
+    },
   ],
 }
